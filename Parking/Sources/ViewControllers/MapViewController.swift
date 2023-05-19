59a121e7cc81409e4af11ec7861f61ca2b04e254
doshkor <dosh.kor@gmail.com>
2023-05-19 13:05:19 +0900

    Chore: Parking 프로젝트 생성

---
+++ Parking/Sources/ViewControllers/MapViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MapViewController.swift
+//  Parking
+//
+//  Created by 신동오 on 2023/05/19.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+}
