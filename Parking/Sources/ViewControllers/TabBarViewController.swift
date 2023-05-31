2aa6b9616a8c0b7a12eb7c5793e2f2f6574f35a7
doshkor <dosh.kor@gmail.com>
2023-05-31 14:08:24 +0900

    Feat: LaunchScreen 이미지 추가, Lottie Package 추가

--- Parking/Sources/ViewControllers/TabBarViewController.swift
+++ Parking/Sources/ViewControllers/TabBarViewController.swift
@@ -6,20 +6,34 @@
 //
 
 import UIKit
+import Lottie
 
 class TabBarViewController: UITabBarController {
     
     // MARK: - Private Property
     
+    private var animationView: LottieAnimationView = {
+        let animationView = LottieAnimationView(name: "pin-animation")
+        animationView.backgroundColor = .white
+        animationView.alpha = 1
+        return animationView
+    }()
+    
+    private var animationBackgroundView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        return view
+    }()
+    
     private let favoriteViewController = FavoriteViewController()
     private let mapViewController = MapViewController()
-    private let viewController = UIViewController()
 
     // MARK: - LifeCycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configure()
+        configureAnimationView()
     }
     
     // MARK: - Private Function
@@ -27,10 +41,11 @@ class TabBarViewController: UITabBarController {
     private func configure() {
         setViewControllers()
         configureUI()
+        configureAnimationView()
     }
     
     private func setViewControllers() {
-        let viewControllers = [favoriteViewController, mapViewController, viewController]
+        let viewControllers = [favoriteViewController, mapViewController]
         self.setViewControllers(viewControllers, animated: true)
         
         favoriteViewController.delegate = mapViewController
@@ -43,18 +58,30 @@ class TabBarViewController: UITabBarController {
             items[1].selectedImage = UIImage(systemName: "map.fill")
             items[1].image = UIImage(systemName: "map")
             items[1].title = "지도"
-            
-            items[2].selectedImage = UIImage(systemName: "gearshape.fill")
-            items[2].image = UIImage(systemName: "gearshape")
-            items[2].title = "설정"
         }
     }
     
     private func configureUI() {
-        viewController.view.backgroundColor = .blue
-        
         tabBar.tintColor = UIConstant.mainUIColor
         tabBar.backgroundColor = .white
+    }
+    
+    private func configureAnimationView() {
+        view.addSubview(animationBackgroundView)
+        animationBackgroundView.frame = view.frame
+        animationBackgroundView.addSubview(animationView)
+        animationView.frame = .init(x: 0, y: 0, width: 50, height: 50)
+        animationView.center = animationBackgroundView.center
+        animationView.alpha = 1
+        
+        animationView.play { _ in
+            UIView.animate(withDuration: 0.3, animations: {
+                self.animationBackgroundView.alpha = 0
+            }, completion: { _ in
+                self.animationBackgroundView.isHidden = true
+                self.animationBackgroundView.removeFromSuperview()
+            })
+        }
     }
 
 }
